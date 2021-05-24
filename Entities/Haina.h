61fd95c67c76536a0848b19f39f8ea4d73cc8616
RuseAlice <alicegeorgiana98@yahoo.com>
2021-05-24 16:31:48 +0300

    am adaugat data la care a fost scoasa haina

--- Entities/Haina.h
+++ Entities/Haina.h
@@ -4,7 +4,7 @@
 
 #ifndef SMARTDROBE_HAINA_H
 #define SMARTDROBE_HAINA_H
-
+#include<ctime>
 enum Culoare{null_culoare,Alb, Negru, Albastru, Verde, Galben, Rosu, Portocaliu, Bleumarin, Gri, Bej, Maro, Mov};
 enum Stil{null_stil,casual, sport, business, formal};
 enum Material{null_material,poliester, bumbac, stofa, denim, matase, in, fas, lana};
@@ -21,27 +21,31 @@ private:
     Material material;
     int nrPurtari=0;
     bool disponibil;
+    time_t ultimaScoatere;
+
 
 public:
     Haina(string denumire, PiesaVestimentara piesaV, Culoare culoare, Stil stil, Material material);
     Haina()=default;
     string getDenumire(){return denumire;}
-     void setDenumire(string denumire){this->denumire=denumire;}
-     PiesaVestimentara getPiesaVestimentara(){return piesaVestimentara;}
-     void setPiesaVestimentara(PiesaVestimentara piesaVestimentara){this->piesaVestimentara=piesaVestimentara;}
-     Culoare getCuloare(){return culoare;}
-     void setCuloare(Culoare culoare){this->culoare=culoare;}
-     Stil getStil(){return stil;};
-     void setStil(Stil stil){this->stil=stil;}
-     Material getMaterial(){return material;}
-     void setMaterial(Material material){this->material=material;}
-     int getNrPurtari(){return nrPurtari;}
-     void setNrPurtari(int nr_purtari){this->nrPurtari=nr_purtari;}
-     bool getDisponibilitate(){return disponibil;}
-     void setDisponibilitate(bool disp){this->disponibil=disp;}
-     bool verificarePotrivire(Culoare culoare, Stil stil, float temperatura);
-     string afisare();
-     bool operator ==(Haina h);
+    void setDenumire(string denumire){this->denumire=denumire;}
+    PiesaVestimentara getPiesaVestimentara(){return piesaVestimentara;}
+    void setPiesaVestimentara(PiesaVestimentara piesaVestimentara){this->piesaVestimentara=piesaVestimentara;}
+    Culoare getCuloare(){return culoare;}
+    void setCuloare(Culoare culoare){this->culoare=culoare;}
+    Stil getStil(){return stil;};
+    void setStil(Stil stil){this->stil=stil;}
+    Material getMaterial(){return material;}
+    void setMaterial(Material material){this->material=material;}
+    int getNrPurtari(){return nrPurtari;}
+    void setNrPurtari(int nr_purtari){this->nrPurtari=nr_purtari;}
+    bool getDisponibilitate(){return disponibil;}
+    void setDisponibilitate(bool disp){this->disponibil=disp;}
+    time_t getUltimaScoatere(){return ultimaScoatere;}
+    void setUltimaScoatere(time_t t){ultimaScoatere=t;}
+    bool verificarePotrivire(Culoare culoare, Stil stil, float temperatura);
+    string afisare();
+    bool operator ==(Haina h);
 
 };
 
