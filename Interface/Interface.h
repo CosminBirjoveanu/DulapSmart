08596f2da0e381bc6ceb4b30b1c89403d35fa711
RuseAlice <alicegeorgiana98@yahoo.com>
2021-05-24 22:55:58 +0300

    umerase

--- Interface/Interface.h
+++ Interface/Interface.h
@@ -8,17 +8,19 @@ class Interface {
 protected:
     language lang;
 public:
-    virtual void mainMenu() = 0;
-    virtual void checkRecommendations() = 0;
-    virtual void checkWeather() = 0;
-    virtual void checkEvents() = 0;
-    virtual void checkConsumables() = 0;
-    virtual void showFreeHangers() = 0;
-    virtual void showControlsAndSettings() = 0;
+    virtual void mainMenu(const int &menuPage) = 0;
+//    virtual void checkRecommendations() = 0;
+//    virtual void checkWeather() = 0;
+//    virtual void checkEvents() = 0;
+//    virtual void checkConsumables() = 0;
+//    virtual void showFreeHangers() = 0;
+//    virtual void showControlsAndSettings() = 0;
 };
 
-class CLI: Interface {
-
+class CLI: public Interface {
+public:
+    void mainMenu(const int &menuPage) override;
+    CLI()=default;
 };
 
 
